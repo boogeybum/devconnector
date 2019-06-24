d008d8ac1ef804c1a4674bab4d1025888580cc60
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-06-24 14:43:58 -0500

    Added code to create new user and validate against existing users to prevent duplicate email addresses. Added dependencies of gravatar and bcrypt

--- config/db.js
+++ config/db.js
@@ -5,15 +5,16 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useCreateIndex: true
     });
 
     console.log('MongoDB Connected...');
-  } catch(err) {
+  } catch (err) {
     console.log(err.message);
     // Exit process with failure
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
